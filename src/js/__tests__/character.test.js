16f4b1613300801e81d20bfdf537c8624935d699
NedorezovNS <N.S.Nedorezov@gmail.com>
2025-09-18 22:01:25 +0300

    edits

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -3,18 +3,18 @@ import Character from "../class/Character";
 
 test('should throw an error if name length is less than 2', () => {
     expect(() => {
-        new Character('B', 'Bowman', 100, 1, 25, 25);
+        new Character('B', 'Bowman', 25, 25);
     }).toThrow('Кол-во символов в name от 2 до 10!');
 });
 
 test('should throw an error if name length is more than 10', () => {
     expect(() => {
-        new Character('RobertoPistruni', 'Bowman', 100, 1, 25, 25);
+        new Character('RobertoPistruni', 'Bowman', 25, 25);
     }).toThrow('Кол-во символов в name от 2 до 10!');
 });
 
 test('should throw an error if type is not valid', () => {
     expect(() => {
-        new Character('Basil', 'Warrior', 100, 1, 25, 25);
+        new Character('Basil', 'Warrior', 25, 25);
     }).toThrow('Данные введены некорректно!');
 });
